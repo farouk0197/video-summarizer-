fea8e4cae53f466dc15b07a786eb977091f9cd08
farouk0197 <fkatakpaou@gmail.com>
2024-07-15 20:41:45 -0400

    install the packages

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import os
+import openai
+from openai import OpenAI
+from dotenv import load_dotenv
+from youtube_transcript_api import YouTubeTranscriptApi
+from pytube import YouTube
+
